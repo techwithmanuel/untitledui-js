b86828b80116e7d96d91a51c5efea7a0bedcb374
Emmanuel Alozie <emmanuelalozie@Emmanuels-MacBook-Pro.local>
2024-06-03 23:09:01 +0100

    feat: generate function components instead of stateless components
    This commit updates the code generation to generate functional components instead of stateless components.
    
    The change addresses the limitations of stateless components, such as the inability to use hooks and the need to define the component's shape in the render function. Functional components offer a more flexible and maintainable approach to component development.
    
    Additionally, the change updates the import syntax to use lazy loading and a fallback component for improved performance and user experience.

--- index.cjs
+++ index.cjs
@@ -22,14 +22,24 @@ function generateExportStatements() {
   const iconFiles = readDirRecursively(iconsDir);
   const exportStatements = iconFiles.map((file) => {
     const fileName = path.basename(file, path.extname(file));
-    return `export { default as ${fileName} } from './icons/${fileName}';`;
+    return `
+   
+    export const ${fileName}:FC<SVGComponentProps> = ({...props}: SVGComponentProps) => {
+      const Icon = lazy(() => import('./icons/${fileName}'))
+
+      return (
+        <Suspense fallback={<DefaultLazyImportFallback/>}>
+        <Icon {...props} />
+        </Suspense>
+      )
+    }`;
   });
   return exportStatements.join("\n");
 }
 
 // Main function
 function main() {
-  const indexPath = path.join(__dirname, "src", "index.ts");
+  const indexPath = path.join(__dirname, "src", "index.tsx");
   const exportStatements = generateExportStatements();
   fs.writeFileSync(indexPath, exportStatements, "utf-8");
   console.log(
